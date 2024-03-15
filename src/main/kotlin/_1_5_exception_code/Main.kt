371f7c580c17aae220f26293c46646266c93a3bf
wily.yang <ehdrnr1178@gmail.com>
2024-03-15 16:26:42 +0900

    5장 진행중

--- src/main/kotlin/_1_5_exception_code/Main.kt
+++ src/main/kotlin/_1_5_exception_code/Main.kt
@@ -2,14 +2,69 @@ package _1_5_exception_code
 
 import kotlinx.coroutines.runBlocking
 
-
-fun pop(num:Int = 1): List<T> {
+fun List<String>.pop(num: Int = 1, isOpen: Boolean): List<String> {
     require(num <= size) {
-        "Cannot remove"
+        "Cannot remove more elements than current size"
     }
+    check(isOpen) { "Cannot pop from closed stack" }
+    val ret = take(num)
+    assert(ret.size == num)
+    return ret
 }
 
+fun factorial(n: Int) : Long {
+    require(n >= 0) {
+        "Cannot calculate factorial of $n " +
+                "because it is smaller than 0"
+    }
+    return if (n <= 1) 1 else factorial((n-1) * n)
+}
 
-suspend fun main() = runBlocking{
-    test1()
+data class Point(val x : Long, val y : Long)
+data class Cluster(val points : List<Point>)
+
+fun findClusters(points: List<Point>) : List<Cluster>{
+    require(points.isNotEmpty())
+    return listOf()
+}
+
+fun isValidEmail(email: String) = email.isEmpty()
+
+data class User(val email : String)
+fun sendEmail(user: User, message: String){
+    requireNotNull(user.email)
+    require(isValidEmail(user.email))
+}
+
+val isInitalized = false
+fun speak(text : String){
+    check(isInitalized)
+}
+
+data class UserInfo(val name : String)
+var token :String? = null
+fun getUserInfo() : UserInfo{
+    checkNotNull(token){
+        "OKOKOK"
+    }
+    return UserInfo(name = "U")
+}
+
+val isOpen = false
+fun next() {
+    check(isOpen)
+}
+
+fun pop(num: Int = 1) : List<String> {
+    val ret = listOf("PPP", "DDD")
+    assert(ret.size == num)
+    return ret
+}
+
+open class Outfit(val name : String)
+
+
+suspend fun main(): Unit = runBlocking{
+//    test1()
+    pop(4)
 }
